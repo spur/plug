c5db651891ed0378a91c070cd13785a8521de5d0
Paul Colleoni <paul.colleoni@gmail.com>
2016-01-29 10:16:21 +0900

    added the last react hooks

--- src/plug.js
+++ src/plug.js
@@ -19,12 +19,23 @@ function plug(plugins, reactComponentClass) {
 
   Plug.prototype = Object.create(reactComponentClass.prototype, {
     constructor: {
-      value: reactComponentClass.name + 'Plug',
+      value: reactComponentClass,
       enumerable: false,
       writable: true,
       configurable: true
     }
   });
+
+  Plug.prototype.componentWillMount = function () {
+    if (reactComponentClass.prototype.componentWillMount) {
+      reactComponentClass.prototype.componentWillMount.call(this);
+    }
+
+    for (var pluginId in this.plugins) {
+      var plugin = this.plugins[pluginId];
+      if (plugin.componentWillMount) { plugin.componentWillMount(); }
+    }
+  };
 
   Plug.prototype.componentDidMount = function () {
     if (reactComponentClass.prototype.componentDidMount) {
@@ -38,25 +49,36 @@ function plug(plugins, reactComponentClass) {
     }
   };
 
-  Plug.prototype.componentWillMount = function () {
-    if (reactComponentClass.prototype.componentWillMount) {
-      reactComponentClass.prototype.componentWillMount.call(this);
+  Plug.prototype.componentWillReceiveProps = function (nextProps) {
+    if (reactComponentClass.prototype.componentWillReceiveProps) {
+      reactComponentClass.prototype.componentWillReceiveProps.call(this, nextProps);
     }
 
     for (var pluginId in this.plugins) {
       var plugin = this.plugins[pluginId];
-      if (plugin.componentWillMount) { plugin.componentWillMount(); }
+      if (plugin.componentWillReceiveProps) { plugin.componentWillReceiveProps(nextProps); }
     }
   };
 
-  Plug.prototype.componentWillReceiveProps = function (nextProps, nextState) {
-    if (reactComponentClass.prototype.componentWillReceiveProps) {
-      reactComponentClass.prototype.componentWillReceiveProps.call(this, nextProps, nextState);
+  Plug.prototype.componentWillUpdate = function (nextProps, nextState) {
+    if (reactComponentClass.prototype.componentWillUpdate) {
+      reactComponentClass.prototype.componentWillUpdate.call(this, nextProps, nextState);
     }
 
     for (var pluginId in this.plugins) {
       var plugin = this.plugins[pluginId];
-      if (plugin.componentWillReceiveProps) { plugin.componentWillReceiveProps(nextProps, nextState); }
+      if (plugin.componentWillUpdate) { plugin.componentWillUpdate(nextProps, nextState); }
+    }
+  };
+
+  Plug.prototype.componentDidUpdate = function () {
+    if (reactComponentClass.prototype.componentDidUpdate) {
+      reactComponentClass.prototype.componentDidUpdate.call(this);
+    }
+
+    for (var pluginId in this.plugins) {
+      var plugin = this.plugins[pluginId];
+      if (plugin.componentDidUpdate) { plugin.componentDidUpdate(); }
     }
   };
 
@@ -72,7 +94,9 @@ function plug(plugins, reactComponentClass) {
   };
 
   for (var staticPropertyKey in reactComponentClass) {
-    Plug[staticPropertyKey] = reactComponentClass[staticPropertyKey];
+    if (reactComponentClass.hasOwnProperty(staticPropertyKey)) {
+      Plug[staticPropertyKey] = reactComponentClass[staticPropertyKey];
+    }
   }
 
   return Plug;
