b385f3b49e81bf1a8d3cd8699cebb1451eadfa58
Paul Colleoni <paul.colleoni@gmail.com>
2016-03-01 12:53:34 +0900

    use a plug name and support plugin dependencies

--- plug.js
+++ plug.js
@@ -1,113 +1,125 @@
 var ReactDom = require('react-dom');
 var objectAssign = require('object-assign');
 
+function installPlugins(plugins, pluginInstances) {
+	for (var i = 0, len = plugins.length; i < len; i += 1) {
+		var Plugin = plugins[i];
+		var plugName = Plugin.plugName || Plugin.name;
+		if (pluginInstances[plugName]) { continue; }
+
+		pluginInstances[plugName] = new Plugin(this);
+
+		var dependencies = Plugin.dependencies;
+		if (!dependencies) {
+			installPlugins(plugins, pluginInstances);
+		}
+	}
+}
+
 function plug(plugins, reactComponentClass) {
-  if (reactComponentClass.reactComponentClass) {
-    reactComponentClass = reactComponentClass.reactComponentClass;
-    plugins = objectAssign({}, plugins, reactComponentClass.plugins);
-  }
+	if (reactComponentClass.reactComponentClass) {
+		reactComponentClass = reactComponentClass.reactComponentClass;
+		plugins = objectAssign({}, plugins, reactComponentClass.plugins);
+	}
 
-  function Plug(props, context) {
-    reactComponentClass.call(this, props, context);
-    var pluginInstances = {};
+	function Plug(props, context) {
+		reactComponentClass.call(this, props, context);
 
-    for (var pluginId in plugins) {
-      pluginInstances[pluginId] = new plugins[pluginId](this);
-    }
+		var pluginInstances = {};
+		installPlugins(plugins, pluginInstances);
+		this.plugins = pluginInstances;
 
-    this.plugins = pluginInstances;
+		if (this.pluginsLoaded) {
+			this.pluginsLoaded();
+		}
+	};
+	Plug.reactComponentClass = reactComponentClass;
+	Plug.plugins = plugins;
 
-    if (this.pluginsLoaded) {
-      this.pluginsLoaded();
-    }
-  };
-  Plug.reactComponentClass = reactComponentClass;
-  Plug.plugins = plugins;
+	Plug.prototype = Object.create(reactComponentClass.prototype, {
+		constructor: {
+			value: reactComponentClass,
+			enumerable: false,
+			writable: true,
+			configurable: true
+		}
+	});
 
-  Plug.prototype = Object.create(reactComponentClass.prototype, {
-    constructor: {
-      value: reactComponentClass,
-      enumerable: false,
-      writable: true,
-      configurable: true
-    }
-  });
+	Plug.prototype.componentWillMount = function () {
+		if (reactComponentClass.prototype.componentWillMount) {
+			reactComponentClass.prototype.componentWillMount.call(this);
+		}
 
-  Plug.prototype.componentWillMount = function () {
-    if (reactComponentClass.prototype.componentWillMount) {
-      reactComponentClass.prototype.componentWillMount.call(this);
-    }
+		for (var pluginId in this.plugins) {
+			var plugin = this.plugins[pluginId];
+			if (plugin.componentWillMount) { plugin.componentWillMount(); }
+		}
+	};
 
-    for (var pluginId in this.plugins) {
-      var plugin = this.plugins[pluginId];
-      if (plugin.componentWillMount) { plugin.componentWillMount(); }
-    }
-  };
+	Plug.prototype.componentDidMount = function () {
+		var DOMNode = ReactDom.findDOMNode(this);
 
-  Plug.prototype.componentDidMount = function () {
-    var DOMNode = ReactDom.findDOMNode(this);
+		if (reactComponentClass.prototype.componentDidMount) {
+			reactComponentClass.prototype.componentDidMount.call(this, DOMNode);
+		}
 
-    if (reactComponentClass.prototype.componentDidMount) {
-      reactComponentClass.prototype.componentDidMount.call(this, DOMNode);
-    }
+		for (var pluginId in this.plugins) {
+			var plugin = this.plugins[pluginId];
+			if (plugin.componentDidMount) { plugin.componentDidMount(DOMNode); }
+		}
+	};
 
-    for (var pluginId in this.plugins) {
-      var plugin = this.plugins[pluginId];
-      if (plugin.componentDidMount) { plugin.componentDidMount(DOMNode); }
-    }
-  };
+	Plug.prototype.componentWillReceiveProps = function (nextProps) {
+		if (reactComponentClass.prototype.componentWillReceiveProps) {
+			reactComponentClass.prototype.componentWillReceiveProps.call(this, nextProps);
+		}
 
-  Plug.prototype.componentWillReceiveProps = function (nextProps) {
-    if (reactComponentClass.prototype.componentWillReceiveProps) {
-      reactComponentClass.prototype.componentWillReceiveProps.call(this, nextProps);
-    }
+		for (var pluginId in this.plugins) {
+			var plugin = this.plugins[pluginId];
+			if (plugin.componentWillReceiveProps) { plugin.componentWillReceiveProps(nextProps); }
+		}
+	};
 
-    for (var pluginId in this.plugins) {
-      var plugin = this.plugins[pluginId];
-      if (plugin.componentWillReceiveProps) { plugin.componentWillReceiveProps(nextProps); }
-    }
-  };
+	Plug.prototype.componentWillUpdate = function (nextProps, nextState) {
+		if (reactComponentClass.prototype.componentWillUpdate) {
+			reactComponentClass.prototype.componentWillUpdate.call(this, nextProps, nextState);
+		}
 
-  Plug.prototype.componentWillUpdate = function (nextProps, nextState) {
-    if (reactComponentClass.prototype.componentWillUpdate) {
-      reactComponentClass.prototype.componentWillUpdate.call(this, nextProps, nextState);
-    }
+		for (var pluginId in this.plugins) {
+			var plugin = this.plugins[pluginId];
+			if (plugin.componentWillUpdate) { plugin.componentWillUpdate(nextProps, nextState); }
+		}
+	};
 
-    for (var pluginId in this.plugins) {
-      var plugin = this.plugins[pluginId];
-      if (plugin.componentWillUpdate) { plugin.componentWillUpdate(nextProps, nextState); }
-    }
-  };
+	Plug.prototype.componentDidUpdate = function () {
+		if (reactComponentClass.prototype.componentDidUpdate) {
+			reactComponentClass.prototype.componentDidUpdate.call(this);
+		}
 
-  Plug.prototype.componentDidUpdate = function () {
-    if (reactComponentClass.prototype.componentDidUpdate) {
-      reactComponentClass.prototype.componentDidUpdate.call(this);
-    }
+		for (var pluginId in this.plugins) {
+			var plugin = this.plugins[pluginId];
+			if (plugin.componentDidUpdate) { plugin.componentDidUpdate(); }
+		}
+	};
 
-    for (var pluginId in this.plugins) {
-      var plugin = this.plugins[pluginId];
-      if (plugin.componentDidUpdate) { plugin.componentDidUpdate(); }
-    }
-  };
+	Plug.prototype.componentWillUnmount = function () {
+		if (reactComponentClass.prototype.componentWillUnmount) {
+			reactComponentClass.prototype.componentWillUnmount.call(this);
+		}
 
-  Plug.prototype.componentWillUnmount = function () {
-    if (reactComponentClass.prototype.componentWillUnmount) {
-      reactComponentClass.prototype.componentWillUnmount.call(this);
-    }
+		for (var pluginId in this.plugins) {
+			var plugin = this.plugins[pluginId];
+			if (plugin.componentWillUnmount) { plugin.componentWillUnmount(); }
+		}
+	};
 
-    for (var pluginId in this.plugins) {
-      var plugin = this.plugins[pluginId];
-      if (plugin.componentWillUnmount) { plugin.componentWillUnmount(); }
-    }
-  };
+	for (var staticPropertyKey in reactComponentClass) {
+		if (reactComponentClass.hasOwnProperty(staticPropertyKey)) {
+			Plug[staticPropertyKey] = reactComponentClass[staticPropertyKey];
+		}
+	}
 
-  for (var staticPropertyKey in reactComponentClass) {
-    if (reactComponentClass.hasOwnProperty(staticPropertyKey)) {
-      Plug[staticPropertyKey] = reactComponentClass[staticPropertyKey];
-    }
-  }
-
-  return Plug;
+	return Plug;
 }
 
 module.exports = plug;
